21e7317bb0890eb0a9d74a8a47b4663688a3e296
Ignacio Kasevich <ignacio@kasevich.com.ar>
2020-07-27 23:23:54 -0300

    Funcionales de intro, addGoggles, y detalles en general

--- arga_settings.hpp
+++ arga_settings.hpp
@@ -24,24 +24,21 @@ ACTIVAR_ROL_AUTOMATICO = 0;                         // Habilita la carga de rol 
                                                     // Para verificar que los roles estan bien escritos activar todas las IAs y entrar
 ACTIVAR_INSIGNIA_AUTOMATICO = 1;                    // Añadir a arga_settings
 INSIGNIA_IR = 0;                                    // establece si las insignias son IR o normales
-                                            
-DEBUG = 1;                                          // Habilita el modo debug
-ENABLE_LOG_SYSTEM = 1;                              // Habilita el sistema de log
-ENABLE_LOG_SYSTEM_ONLY_DEDICATED = 1;               // Habilita el sistema de log sólo si el servidor es dedicado (requiere ENABLE_LOG_SYSTEM = 1)
 
-//EQUIPAMIENTO
-// Camuflajes disponibles:          Tipos disponibles:
-//      - Bosque                        - Multicam 
-//      - Desierto                      - Marpat
-//      - Selva                         - Liso
-//      - Negro
-//
-//      - Personalizado (se debe ingresar mas abajo la clase del uniforme, chaleco, casco y mochila)
-//      - No editar (no se le modificara ninguna de las 4 cosas al jugador, solo arma y equipo interno)
-//
-//        Las opciones Personalizada y No editar pueden llevar a comportamientos inesperados.
-//        Por lo que el editor queda a cargo de checkear y revisar el correcto funcionamiento.
+/* 
+EQUIPAMIENTO
+    Camuflajes disponibles:          Tipos disponibles:
+       - Bosque                        - Multicam 
+       - Desierto                      - Marpat
+       - Selva                         - Liso
+       - Negro
 
+       - Personalizado (se debe ingresar mas abajo la clase del uniforme, chaleco, casco y mochila)
+       - No editar (no se le modificara ninguna de las 4 cosas al jugador, solo arma y equipo interno)
+
+         Las opciones Personalizada y No editar pueden llevar a comportamientos inesperados.
+         Por lo que el editor queda a cargo de checkear y revisar el correcto funcionamiento.
+*/
 CAMUFLAJE = ["Negro", "Multicam"];
 GAFAS_INICIALES = "";
 UNIFORME_PERSONALIZADO = "";
@@ -49,10 +46,13 @@ CASCO_PERSONALIZADO = "";
 CHALECO_PERSONALIZADO = "";
 MOCHILA_PERSONALIZADO = "";
 MOCHILA_CHICA_PERSONALIZADO = "";
+////////////////////////////////////////////////////////////////////////////////
 
-//BRIEFING
-// Cada elemento del array es un parrafo
-// Ejemplo: ["Primer parrafo", "Segundo parrafo"]
+/* 
+BRIEFING
+    Cada elemento del array es un parrafo
+    Ejemplo: ["Primer parrafo", "Segundo parrafo"] 
+*/
 CARACTER = "No Oficial";
 HORARIO = "";
 FECHA = "";
@@ -66,7 +66,7 @@ RECURSOS = ["", ""];
 ENEMIGOS = ["", ""];
 ////////////////////////////////////////////////////////////////////////////////
 
-//IA - CONFIGURACION
+/* IA - CONFIGURACION */
 DESACTIVAR_IA_DE_GRUPO = 1; // 0 No desactiva la IA del Grupo de los jugadores, 1 la desactiva.
 DESACTIVAR_TODO_BLUFOR = 1; // 0 No desactiva la IA del Blufor, 1 la desactiva.
 DIFICULTAD_IA_PERSONALIZADA = 0;
@@ -89,13 +89,16 @@ overviewPicture = IMAGE;                    // Imagen en seleccion de mision
 loadScreen = IMAGE;                         // Imagen en carga
 ////////////////////////////////////////////////////////////////////////////////
 
-AUXILIAR_ROLE_LIST = [["lider","Líder",34],["sub_lider","Sub Líder",35],["lider_peloton","Líder Pelotón",36],["ametrallador_m240","Ametrallador m240",37],["ametrallador_m249","Ametrallador m249",38],["granadero","Granadero",39],["granadero_bengalas","Granadero bengalas",40],["lanzador_at","Lanzador AT",41],["lanzador_aa","Lanzador AA",42],["tirador_fal","Tirador fal",43],["tirador_mk11","Tirador mk11",44],["francotirador","Francotirador",45],["ingeniero","Ingeniero",46],["observador","Observador",47],["piloto","Piloto",48],["radio_operador","Radio operador",49],["medico","Médico",50],["medico_combate","Médico de combate",51],["enfermero","Enfermero",52],["fusilero","Fusilero",53],["fusilero_ra","Fusilero RA",54],["entrenamiento","Entrenamiento",55],["desconocido","Desconocido",56],["lider","Cacique",1],["entrenamiento","Soldado",2],["sub_lider","teniente",3],["lider","Capitan",4],["sub_lider","Teniente ¹",5],["radio_operador","Radio operador",7],["lider_peloton","Lider de Escuadra",8],["fusilero","Fusilero",9],["ametrallador_m249","Ametrallador",13],["granadero","Granadero",15],["ingeniero","Ingeniero",17],["lanzador_at","Anti Tanque",19],["tirador_mk11","Tirador",22],["enfermero","Enfermero",24],["medico","Medico",25],["piloto","Piloto",62],["lanzador_at","AT",71],["ingeniero","Sombra Especialista",72],["medico","Sombra Medico",73],["fusilero","Sombra",74],["lider_peloton","Lider Sombra",75],["sub_lider","Teniente Primero",148],["lanzador_at","Fusilero AT",149],["lider","Lider",193],["lanzador_aa","AA",197],["radio_operador","Fusilero Ro",204],["ametrallador_m240","Ametrallador 762",213],["tirador_fal","Tirador Designado CP",228],["sub_lider","Sub Lider",238],["entrenamiento","Cabo Primero",314],["entrenamiento","Instructor",321],["entrenamiento","Soldado Primero",327],["entrenamiento","Cabo",328],["entrenamiento","Recluta",337],["entrenamiento","Sargento Primero",356],["entrenamiento","Aspirante",357],["fusilero_ra","Fusilero R/A",391],["tirador_mk11","Tirador Designado",394],["lider_peloton","Matrero",396],["ametrallador_m249","Ametrallador 556",401],["sub_lider","Pampa",422],["desconocido","PRUEBA 3",463],["desconocido","any",471],["lanzador_aa","Fusilero AA",495],["lider_peloton","Lider-Tirador",502],["sub_lider","SubLider",672],["entrenamiento","Comandante",739],["entrenamiento","Sargento",769],["entrenamiento","PRUEBA 1",831],["entrenamiento","PRUEBA 2",833],["entrenamiento","Teniente¹",932]];
+/* Log system */
+DEBUG = 1;                                          // Habilita el modo debug
+ENABLE_LOG_SYSTEM = 1;                              // Habilita el sistema de log
+ENABLE_LOG_SYSTEM_ONLY_DEDICATED = 1;               // Habilita el sistema de log sólo si el servidor es dedicado (requiere ENABLE_LOG_SYSTEM = 1)
 ////////////////////////////////////////////////////////////////////////////////
 
+/* Listas auxiliares */
+AUXILIAR_ROLE_LIST = [["lider","Líder",34],["sub_lider","Sub Líder",35],["lider_peloton","Líder Pelotón",36],["ametrallador_m240","Ametrallador m240",37],["ametrallador_m249","Ametrallador m249",38],["granadero","Granadero",39],["granadero_bengalas","Granadero bengalas",40],["lanzador_at","Lanzador AT",41],["lanzador_aa","Lanzador AA",42],["tirador_fal","Tirador fal",43],["tirador_mk11","Tirador mk11",44],["francotirador","Francotirador",45],["ingeniero","Ingeniero",46],["observador","Observador",47],["piloto","Piloto",48],["radio_operador","Radio operador",49],["medico","Médico",50],["medico_combate","Médico de combate",51],["enfermero","Enfermero",52],["fusilero","Fusilero",53],["fusilero_ra","Fusilero RA",54],["entrenamiento","Entrenamiento",55],["desconocido","Desconocido",56],["lider","Cacique",1],["entrenamiento","Soldado",2],["sub_lider","teniente",3],["lider","Capitan",4],["sub_lider","Teniente ¹",5],["radio_operador","Radio operador",7],["lider_peloton","Lider de Escuadra",8],["fusilero","Fusilero",9],["ametrallador_m249","Ametrallador",13],["granadero","Granadero",15],["ingeniero","Ingeniero",17],["lanzador_at","Anti Tanque",19],["tirador_mk11","Tirador",22],["enfermero","Enfermero",24],["medico","Medico",25],["piloto","Piloto",62],["lanzador_at","AT",71],["ingeniero","Sombra Especialista",72],["medico","Sombra Medico",73],["fusilero","Sombra",74],["lider_peloton","Lider Sombra",75],["sub_lider","Teniente Primero",148],["lanzador_at","Fusilero AT",149],["lider","Lider",193],["lanzador_aa","AA",197],["radio_operador","Fusilero Ro",204],["ametrallador_m240","Ametrallador 762",213],["tirador_fal","Tirador Designado CP",228],["sub_lider","Sub Lider",238],["entrenamiento","Cabo Primero",314],["entrenamiento","Instructor",321],["entrenamiento","Soldado Primero",327],["entrenamiento","Cabo",328],["entrenamiento","Recluta",337],["entrenamiento","Sargento Primero",356],["entrenamiento","Aspirante",357],["fusilero_ra","Fusilero R/A",391],["tirador_mk11","Tirador Designado",394],["lider_peloton","Matrero",396],["ametrallador_m249","Ametrallador 556",401],["sub_lider","Pampa",422],["desconocido","PRUEBA 3",463],["desconocido","any",471],["lanzador_aa","Fusilero AA",495],["lider_peloton","Lider-Tirador",502],["sub_lider","SubLider",672],["entrenamiento","Comandante",739],["entrenamiento","Sargento",769],["entrenamiento","PRUEBA 1",831],["entrenamiento","PRUEBA 2",833],["entrenamiento","Teniente¹",932]];
 ARGA_PLATOON_LIST = [["condor",["condor","cóndor"]],["yaguar",["yaguar","yaguarete","yaguareté"]]];
 AVAILABLE_INSIGNIA_TYPES = ["arga","condor","yaguar","medico"];
-////////////////////////////////////////////////////////////////////////////////
-
 BOX_ROLE_LIST = ["Ametrallador M240","Ametrallador M249","Capitan","Enfermero","Francotirador","Fusilero","Fusilero RA","Granadero","Granadero c/Bengalas","Ingeniero","Lanzador AA","Lanzador AT","Líder","Médico de Combate","Médico","Observador","Piloto","Radio Operador","Teniente","Tirador FAL","Tirador MK11"];
 ////////////////////////////////////////////////////////////////////////////////
 
