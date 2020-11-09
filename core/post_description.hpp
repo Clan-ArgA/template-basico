a76eedffba9c517dc247a77d21b0b9f0a73dfc45
Ignacio Kasevich <ignacio@kasevich.com.ar>
2020-11-09 20:08:02 -0300

    fix aa, change version to push master v0.9.11

--- core/post_description.hpp
+++ core/post_description.hpp
@@ -2,13 +2,13 @@
                           Realizado por |ArgA|MIV
 *******************************************************************************/
 
-class Extended_InitPost_EventHandlers {
-    class CAManBase {
-        class initpost_units {
-            init = "_this call compile preprocessFileLineNumbers 'core\scripts\init_man.sqf'";
-        };
-    };
-};
+// class Extended_InitPost_EventHandlers {
+//     class CAManBase {
+//         class initpost_units {
+//             init = "_this call compile preprocessFileLineNumbers 'core\scripts\init_man.sqf'";
+//         };
+//     };
+// };
 
 /*******************************************************************************
                           Realizado por |ArgA|MIV
