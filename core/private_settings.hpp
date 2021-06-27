7d9b182bdba22ff1050b1a8522e622b5f7be5199
VulturARG <luisbriones@gmail.com>
2021-06-27 10:22:51 -0300

    Template Date and version updated

--- core/private_settings.hpp
+++ core/private_settings.hpp
@@ -2,8 +2,8 @@
                           Realizado por |ArgA|MIV
 *******************************************************************************/
 
-TEMPLATE_VERSION     = "0.9.18";
-TEMPLATE_LAST_UPDATE = "2020-2-20";                  // Escribir la fecha en formato AAAA-M-D sin ceros en el mes o el día.
+TEMPLATE_VERSION     = "0.9.19";
+TEMPLATE_LAST_UPDATE = "2020-6-27";                  // Escribir la fecha en formato AAAA-M-D sin ceros en el mes o el día.
 
 /* Log system */
 DEBUG = 1;                                          // 1: Habilita el modo debug, 0: Deshabilita.
